c2c632639e0a8f8ab32f05f700657ce732c2360e
Tarrence van As <tarrence@vanas.family>
2022-08-09 16:51:18 -0400

    test: add additional test case

--- tests/gen.py
+++ tests/gen.py
@@ -25,69 +25,83 @@ def split(G):
 
     return (G0, G1, G2)
 
-pubkey = decode_credential_public_key(base64url_to_bytes("pQECAyYgASFYIPofnDyxJWqz1XWBumSQ5qEeUQ3Wfaug1hXgVAoXO0S/Ilggl5qh3cisV9IZ+Y9Z8I1jqMH09TOtRxTwPxc9X4cJpwc="))
-challenge = b"0x044e3adc845e501b01c6904dd2b0cd0d084bb01240966d39c3165481dfcae65w"
-authenticator_data_bytes = bytes.fromhex("20a97ec3f8efbc2aca0cf7cabb420b4a09d0aec9905466c9adf79584fa75fed30500000000")
-client_data_bytes = bytes.fromhex("7b2274797065223a22776562617574686e2e676574222c226368616c6c656e6765223a22307830343465336164633834356535303162303163363930346464326230636430643038346262303132343039363664333963333136353438316466636165363577222c226f726967696e223a2268747470733a2f2f636f6e74726f6c6c65722d6531337074397777762e707265766965772e6361727472696467652e6767222c2263726f73734f726967696e223a66616c73657d")
-signature = "3045022024c32f5128206df9fa8102e0f7a2a908b8cae76fee717d50ecaa95eeb125199302210096f6daa99ca841cc5064d72b1f9d6f11154909cf7b324312b82f2cc8736728c3"
+data = [{
+    'pubkey': "pQECAyYgASFYIPofnDyxJWqz1XWBumSQ5qEeUQ3Wfaug1hXgVAoXO0S/Ilggl5qh3cisV9IZ+Y9Z8I1jqMH09TOtRxTwPxc9X4cJpwc=",
+    'challenge': b"0x044e3adc845e501b01c6904dd2b0cd0d084bb01240966d39c3165481dfcae65w",
+    'authenticator_data': "20a97ec3f8efbc2aca0cf7cabb420b4a09d0aec9905466c9adf79584fa75fed30500000000",
+    # {"type":"webauthn.get","challenge":"0x044e3adc845e501b01c6904dd2b0cd0d084bb01240966d39c3165481dfcae65w","origin":"https://controller-e13pt9wwv.preview.cartridge.gg","crossOrigin":false}
+    'client_data': "7b2274797065223a22776562617574686e2e676574222c226368616c6c656e6765223a22307830343465336164633834356535303162303163363930346464326230636430643038346262303132343039363664333963333136353438316466636165363577222c226f726967696e223a2268747470733a2f2f636f6e74726f6c6c65722d6531337074397777762e707265766965772e6361727472696467652e6767222c2263726f73734f726967696e223a66616c73657d",
+    'signature': "3045022024c32f5128206df9fa8102e0f7a2a908b8cae76fee717d50ecaa95eeb125199302210096f6daa99ca841cc5064d72b1f9d6f11154909cf7b324312b82f2cc8736728c3"
+}, {
+    'pubkey': "pQECAyYgASFYILy4sqBJQupsn3ttx5Af1lK8i75VKbji6u5EqOVkHzUaIlggABPNyK5iEFpOWlFjtoZuW8rVDoLLTIar7jFXqGIVaZU=",
+    'challenge': b"0xb144c903c4d9b0bb5d6526b8ca03380b82a28283562f98f6f14a2298052463",
+    'authenticator_data': "20a97ec3f8efbc2aca0cf7cabb420b4a09d0aec9905466c9adf79584fa75fed30500000000",
+    'client_data': "7b2274797065223a22776562617574686e2e676574222c226368616c6c656e6765223a2230786231343463393033633464396230626235643635323662386361303333383062383261323832383335363266393866366631346132323938303532343633222c226f726967696e223a2268747470733a2f2f636f6e74726f6c6c65722d6769742d74617272656e63652d656e672d3139352d63726564656e7469616c2d726567697374726174696f6e2d3937363639372e707265766965772e6361727472696467652e6767222c2263726f73734f726967696e223a66616c73657d",
+    'signature': "30440220513f88733910b29d0c153d12528efde67c76ad40ebded620295e9b2cbc4df9fb02201b6daf037a627ea8ae446305052c473824c5b88097deb233a30a2ea5236db97a"
+}]
 
-x0, x1, x2 = split(int.from_bytes(pubkey.x, "big"))
-y0, y1, y2 = split(int.from_bytes(pubkey.y, "big"))
+for item in data:
+    pubkey = decode_credential_public_key(base64url_to_bytes(item["pubkey"]))
+    authenticator_data_bytes = bytes.fromhex(item["authenticator_data"])
+    client_data_bytes = bytes.fromhex(item["client_data"])
 
-client_data_hash = hashlib.sha256()
-client_data_hash.update(client_data_bytes)
-client_data_hash_bytes = client_data_hash.digest()
+    x0, x1, x2 = split(int.from_bytes(pubkey.x, "big"))
+    y0, y1, y2 = split(int.from_bytes(pubkey.y, "big"))
 
-client_data_rem = len(client_data_bytes) % 4
-for _ in range(4 - client_data_rem):
-    client_data_bytes = client_data_bytes + b'\x00'
+    client_data_hash = hashlib.sha256()
+    client_data_hash.update(client_data_bytes)
+    client_data_hash_bytes = client_data_hash.digest()
 
-authenticator_data_rem = len(authenticator_data_bytes) % 4
+    client_data_rem = len(client_data_bytes) % 4
+    for _ in range(4 - client_data_rem):
+        client_data_bytes = client_data_bytes + b'\x00'
 
-authenticator_data = [int.from_bytes(authenticator_data_bytes[i:i+4], 'big') for i in range(0, len(authenticator_data_bytes), 4)]
-client_data_json = [int.from_bytes(client_data_bytes[i:i+4], 'big') for i in range(0, len(client_data_bytes), 4)]
+    authenticator_data_rem = len(authenticator_data_bytes) % 4
 
-sig, rest = der_decoder(binascii.unhexlify(signature), asn1Spec=DERSig())
-if len(rest) != 0:
-    raise Exception('Bad encoding')
+    authenticator_data = [int.from_bytes(authenticator_data_bytes[i:i+4], 'big') for i in range(0, len(authenticator_data_bytes), 4)]
+    client_data_json = [int.from_bytes(client_data_bytes[i:i+4], 'big') for i in range(0, len(client_data_bytes), 4)]
 
-r0, r1, r2 = split(int(sig['r']))
-s0, s1, s2 = split(int(sig['s']))
+    sig, rest = der_decoder(binascii.unhexlify(item["signature"]), asn1Spec=DERSig())
+    if len(rest) != 0:
+        raise Exception('Bad encoding')
 
-authenticator_data_parts = [int.from_bytes(authenticator_data_bytes[i:i+4], 'big') for i in range(0, len(authenticator_data_bytes), 4)]
-challenge_parts = [int.from_bytes(challenge[i:i+4], 'big') for i in range(0, len(challenge), 4)]
+    r0, r1, r2 = split(int(sig['r']))
+    s0, s1, s2 = split(int(sig['s']))
 
-# {"type":"webauthn.get","challenge":"0x044e3adc845e501b01c6904dd2b0cd0d084bb01240966d39c3165481dfcae65w","origin":"https://controller-e13pt9wwv.preview.cartridge.gg","crossOrigin":false}
-origin = b'https://controller-e13pt9wwv.preview.cartridge.gg'
-origin_parts = [int.from_bytes(origin[i:i+4], 'big') for i in range(0, len(origin), 4)]
-origin_offset_bytes = client_data_bytes.find(b'"origin":"') + len(b'"origin":"')
+    authenticator_data_parts = [int.from_bytes(authenticator_data_bytes[i:i+4], 'big') for i in range(0, len(authenticator_data_bytes), 4)]
+    challenge_parts = [int.from_bytes(item["challenge"][i:i+4], 'big') for i in range(0, len(item["challenge"]), 4)]
 
-type = b'webauthn.get'
-type_parts = [int.from_bytes(type[i:i+4], 'big') for i in range(0, len(type), 4)]
-type_offset_bytes = client_data_bytes.find(b'"type":"') + len(b'"type":"')
+    origin = b'https://controller-e13pt9wwv.preview.cartridge.gg'
+    origin_parts = [int.from_bytes(origin[i:i+4], 'big') for i in range(0, len(origin), 4)]
+    origin_offset_bytes = client_data_bytes.find(b'"origin":"') + len(b'"origin":"')
 
-challenge_offset_bytes = client_data_bytes.find(b'"challenge":"') + len(b'"challenge":"')
+    type = b'webauthn.get'
+    type_parts = [int.from_bytes(type[i:i+4], 'big') for i in range(0, len(type), 4)]
+    type_offset_bytes = client_data_bytes.find(b'"type":"') + len(b'"type":"')
 
-print("x", x0, x1, x2)
-print("y", y0, y1, y2)
-print("r", r0, r1, r2)
-print("s", s0, s1, s2)
-print("callenge_rem", 4 - (len(challenge) % 4))
-print("challenge_parts_len", len(challenge_parts))
-print("challenge_parts", challenge_parts)
-print("origin_offset_len", origin_offset_bytes // 4)
-print("origin_offset_rem", origin_offset_bytes % 4)
-print("origin_len", len(origin_parts))
-print("origin", origin_parts)
-print("type_parts", type_parts)
-print("type_offset_len", type_offset_bytes // 4)
-print("type_offset_rem", type_offset_bytes % 4)
-print("challenge_offset_len", challenge_offset_bytes // 4)
-print("challenge_offset_rem", challenge_offset_bytes % 4)
-print("client_dat_json", client_data_bytes)
-print("client_data_json_parts", client_data_json)
-print("client_data_json_len", len(client_data_json))
-print("client_data_json_rem", 4 - client_data_rem)
-print("authenticator_data_parts", authenticator_data_parts)
-print("authenticator_data_len", len(authenticator_data_parts))
-print("authenticator_data_rem", 4 - authenticator_data_rem)
+    challenge_offset_bytes = client_data_bytes.find(b'"challenge":"') + len(b'"challenge":"')
+
+    print("x", x0, x1, x2)
+    print("y", y0, y1, y2)
+    print("r", r0, r1, r2)
+    print("s", s0, s1, s2)
+    print("callenge_rem", 4 - (len(item["challenge"]) % 4))
+    print("challenge_parts_len", len(challenge_parts))
+    print("challenge_parts", challenge_parts)
+    print("challenge_offset_len", challenge_offset_bytes // 4)
+    print("challenge_offset_rem", challenge_offset_bytes % 4)
+    print("origin_offset_len", origin_offset_bytes // 4)
+    print("origin_offset_rem", origin_offset_bytes % 4)
+    print("origin_len", len(origin_parts))
+    print("origin", origin_parts)
+    print("type_parts", type_parts)
+    print("type_offset_len", type_offset_bytes // 4)
+    print("type_offset_rem", type_offset_bytes % 4)
+    print("client_dat_json", client_data_bytes)
+    print("client_data_json_parts", client_data_json)
+    print("client_data_json_len", len(client_data_json))
+    print("client_data_json_rem", 4 - client_data_rem)
+    print("authenticator_data_parts", authenticator_data_parts)
+    print("authenticator_data_len", len(authenticator_data_parts))
+    print("authenticator_data_rem", 4 - authenticator_data_rem)
+    print("\n\n")
--- tests/test_webauthn.cairo
+++ tests/test_webauthn.cairo
@@ -8,7 +8,7 @@ from src.bigint import BigInt3
 from src.webauthn import Webauthn
 
 @external
-func test_verify{syscall_ptr : felt*, range_check_ptr, bitwise_ptr: BitwiseBuiltin*}():
+func test_verify_1{syscall_ptr : felt*, range_check_ptr, bitwise_ptr: BitwiseBuiltin*}():
     let public_key_pt = EcPoint(
         BigInt3(52743338060272243486442687, 8356564404182767302564342, 18898795645626225295972184),
         BigInt3(8561359831707316770154247, 74234461271779437484232373, 11454876819198723055787928),
@@ -110,6 +110,145 @@ func test_verify{syscall_ptr : felt*, range_check_ptr, bitwise_ptr: BitwiseBuilt
     assert client_data_json[44] = 1847736934
     assert client_data_json[45] = 1634497381
     assert client_data_json[46] = 2097152000
+
+    let authenticator_data_len = 10
+    let authenticator_data_rem = 3
+    let (authenticator_data) = alloc()
+    assert authenticator_data[0] = 547978947
+    assert authenticator_data[1] = 4176460842
+    assert authenticator_data[2] = 3389847498
+    assert authenticator_data[3] = 3141667658
+    assert authenticator_data[4] = 164671177
+    assert authenticator_data[5] = 2421450441
+    assert authenticator_data[6] = 2918684036
+    assert authenticator_data[7] = 4202036947
+    assert authenticator_data[8] = 83886080
+    assert authenticator_data[9] = 0
+
+    Webauthn.verify(public_key_pt, r, s,
+        type_offset_len, type_offset_rem,
+        challenge_offset_len, challenge_offset_rem, challenge_len, challenge_rem, challenge,
+        origin_offset_len, origin_offset_rem, origin_len, origin,
+        client_data_json_len, client_data_json_rem, client_data_json,
+        authenticator_data_len, authenticator_data_rem, authenticator_data
+    )
+
+    return ()
+end
+
+@external
+func test_verify_2{syscall_ptr : felt*, range_check_ptr, bitwise_ptr: BitwiseBuiltin*}():
+    let public_key_pt = EcPoint(
+        BigInt3(68771434430694404260836634, 66633780841359531151283366, 14259391536917482115299254),
+        BigInt3(15143078500912722485799317, 6719228382483657703164717, 5845062894556851529125),
+    )
+    let r = BigInt3(37278954292573957828508155, 63210244753363287212884911, 6138938596067845166121299)
+    let s = BigInt3(37109308354591296024590714, 14601938862236783601648223, 2072435219191356476548166)
+
+    let type_offset_len = 2
+    let type_offset_rem = 1
+
+    let challenge_offset_len = 9
+    let challenge_offset_rem = 0
+    let challenge_len = 16
+    let challenge_rem = 0
+    let (challenge) = alloc()
+    assert challenge[0] = 813195825
+    assert challenge[1] = 875848505
+    assert challenge[2] = 808674100
+    assert challenge[3] = 1681482288
+    assert challenge[4] = 1650603364
+    assert challenge[5] = 909455926
+    assert challenge[6] = 1647862625
+    assert challenge[7] = 808661816
+    assert challenge[8] = 811743282
+    assert challenge[9] = 1630681138
+    assert challenge[10] = 942880054
+    assert challenge[11] = 845560120
+    assert challenge[12] = 1714841137
+    assert challenge[13] = 878785074
+    assert challenge[14] = 959983669
+    assert challenge[15] = 842282547
+
+    let origin_offset_len = 28
+    let origin_offset_rem = 2
+    let origin_len = 13
+    let (origin) = alloc()
+    assert origin[0] = 1752462448
+    assert origin[1] = 1933193007
+    assert origin[2] = 1668247156
+    assert origin[3] = 1919904876
+    assert origin[4] = 1701981541
+    assert origin[5] = 825454708
+    assert origin[6] = 964130678
+    assert origin[7] = 779121253
+    assert origin[8] = 1986618743
+    assert origin[9] = 778264946
+    assert origin[10] = 1953655140
+    assert origin[11] = 1734684263
+    assert origin[12] = 103
+
+    let client_data_json_len = 57
+    let client_data_json_rem = 3
+    let (client_data_json) = alloc()
+    assert client_data_json[0] = 2065855609
+    assert client_data_json[1] = 1885676090
+    assert client_data_json[2] = 578250082
+    assert client_data_json[3] = 1635087464
+    assert client_data_json[4] = 1848534885
+    assert client_data_json[5] = 1948396578
+    assert client_data_json[6] = 1667785068
+    assert client_data_json[7] = 1818586727
+    assert client_data_json[8] = 1696741922
+    assert client_data_json[9] = 813195825
+    assert client_data_json[10] = 875848505
+    assert client_data_json[11] = 808674100
+    assert client_data_json[12] = 1681482288
+    assert client_data_json[13] = 1650603364
+    assert client_data_json[14] = 909455926
+    assert client_data_json[15] = 1647862625
+    assert client_data_json[16] = 808661816
+    assert client_data_json[17] = 811743282
+    assert client_data_json[18] = 1630681138
+    assert client_data_json[19] = 942880054
+    assert client_data_json[20] = 845560120
+    assert client_data_json[21] = 1714841137
+    assert client_data_json[22] = 878785074
+    assert client_data_json[23] = 959983669
+    assert client_data_json[24] = 842282547
+    assert client_data_json[25] = 573317743
+    assert client_data_json[26] = 1919510377
+    assert client_data_json[27] = 1847736866
+    assert client_data_json[28] = 1752462448
+    assert client_data_json[29] = 1933193007
+    assert client_data_json[30] = 1668247156
+    assert client_data_json[31] = 1919904876
+    assert client_data_json[32] = 1701981543
+    assert client_data_json[33] = 1769221492
+    assert client_data_json[34] = 1634890341
+    assert client_data_json[35] = 1852007725
+    assert client_data_json[36] = 1701734189
+    assert client_data_json[37] = 825832749
+    assert client_data_json[38] = 1668441444
+    assert client_data_json[39] = 1701737577
+    assert client_data_json[40] = 1634479474
+    assert client_data_json[41] = 1701276019
+    assert client_data_json[42] = 1953653108
+    assert client_data_json[43] = 1768910381
+    assert client_data_json[44] = 959919670
+    assert client_data_json[45] = 959917680
+    assert client_data_json[46] = 1919252073
+    assert client_data_json[47] = 1702309475
+    assert client_data_json[48] = 1634890866
+    assert client_data_json[49] = 1768187749
+    assert client_data_json[50] = 778528546
+    assert client_data_json[51] = 740451186
+    assert client_data_json[52] = 1869837135
+    assert client_data_json[53] = 1919510377
+    assert client_data_json[54] = 1847736934
+    assert client_data_json[55] = 1634497381
+    assert client_data_json[56] = 2097152000
+
 
     let authenticator_data_len = 10
     let authenticator_data_rem = 3
