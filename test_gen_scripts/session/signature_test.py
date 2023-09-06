cdfc45f27b28c455f0cada198a0dca639adf50c8
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2023-09-06 16:13:33 +0000

    into signature tests

--- test_gen_scripts/session/signature_test.py
+++ test_gen_scripts/session/signature_test.py
@@ -12,7 +12,12 @@ class SignatureTest(TestFileCreatorInterface):
     def test_file(self, python_source_folder: str) -> TestFile:
         tf = TestFile("signature", python_source_folder)
         tf.add_imports(self.get_imports())
-        tf.add_block(self.get_test())
+        for name, count, size, token in [
+            ('small', 2, 3, 5),
+            ('medium', 10, 10, 40),
+            ('large', 100, 10, 100)
+        ]:
+            tf.add_block(self.get_test(name, count, size, token))
         return tf
 
     def get_imports(self):
@@ -21,25 +26,43 @@ class SignatureTest(TestFileCreatorInterface):
             "core::option::OptionTrait",
             "array::ArrayTrait",
             "core::pedersen::pedersen",
-            "webauthn_session::signature::FeltSpanTryIntoSignature"
+            "webauthn_session::signature::FeltSpanTryIntoSignature",
+            "webauthn_session::signature::ImplSignatureProofs"
         ]
     
-    def get_test(self):
+    def get_test(
+            self, 
+            name,
+            number_of_proofs, 
+            single_proof_len, 
+            session_token_len
+        ):
         r = 1830
         s = 1863
         session_key = 1918
         session_expires = 1694114435
         root = 1922
-        proof_len = 2
-        proofs = [i for i in range(proof_len * 2)]
-        session_token = [i for i in range(5)]
+        proofs = [i * 100 for i in range(single_proof_len * number_of_proofs)]
+        session_token = [i + 2000 for i in range(session_token_len)]
 
         sig = [
-            0, r, s, session_key, session_expires, root, proof_len, len(proofs)
+            0, r, s, session_key, session_expires, root, single_proof_len, len(proofs)
         ] + proofs + [
             len(session_token)
         ] + session_token
 
         body = iterable_as_cairo_array(sig, 'sig', 'felt252')
-        body += f'FeltSpanTryIntoSignature::try_into(sig.span()).unwrap();'
-        return Test('span_into_signature', body)
+        body += iterable_as_cairo_array(proofs, 'proofs', 'felt252')
+        body += iterable_as_cairo_array(session_token, 'session_token', 'felt252')
+        body += f'let sig = FeltSpanTryIntoSignature::try_into(sig.span()).unwrap();\n'
+        body += '\n'.join([
+            f"assert({r} == sig.r, 'Expect equal!');",
+            f"assert({s} == sig.s, 'Expect equal!');",
+            f"assert({session_key} == sig.session_key, 'Expect equal!');",
+            f"assert({root} == sig.root, 'Expect equal!');",
+            f"assert({number_of_proofs} == sig.proofs.len(), 'Expect equal!');",
+            f"assert({session_token_len} == sig.session_token.len(), 'Expect equal!');",
+            f"assert({proofs[-1]} == *sig.proofs.at({number_of_proofs - 1}).at({single_proof_len - 1}), 'Expect equal!');",
+            f"assert({session_token[-1]} == *sig.session_token.at({session_token_len - 1}), 'Expect equal!');"
+        ])
+        return Test(f'span_into_signature_{name}', body)
