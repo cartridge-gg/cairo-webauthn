d81a3572daefbcad3df0b98aa3cc88acadf1297f
Mateusz Zając <matzayonc@gmail.com>
2023-11-16 19:20:27 +0000

    added deployment

--- account_sdk/src/deploy_contract.rs
+++ account_sdk/src/deploy_contract.rs
@@ -2,25 +2,28 @@ use std::sync::Arc;
 
 use crate::rpc_provider::RpcClientProvider;
 use starknet::{
-    accounts::{Account, ExecutionEncoding, SingleOwnerAccount},
+    accounts::{Account, Call, ExecutionEncoding, SingleOwnerAccount},
     core::types::{
         contract::{CompiledClass, SierraClass},
-        DeclareTransactionResult, FieldElement, BlockId, BlockTag,
+        BlockId, BlockTag, DeclareTransactionResult, FieldElement,
     },
+    macros::{felt, selector},
     providers::{JsonRpcClient, Provider},
     signers::{LocalWallet, SigningKey},
 };
 
-const SIERRA_STR: &str =
-    include_str!("../../cartridge_account/target/dev/cartridge_account_Account.contract_class.json");
-const CASM_STR: &str =
-    include_str!("../../cartridge_account/target/dev/cartridge_account_Account.compiled_contract_class.json");
+const SIERRA_STR: &str = include_str!(
+    "../../cartridge_account/target/dev/cartridge_account_Account.contract_class.json"
+);
+const CASM_STR: &str = include_str!(
+    "../../cartridge_account/target/dev/cartridge_account_Account.compiled_contract_class.json"
+);
 
 pub async fn deploy_contract<T>(
     rpc_provider: impl RpcClientProvider<T>,
     signing_key: SigningKey,
     address: FieldElement,
-) -> Result<DeclareTransactionResult, String>
+) -> Result<(), String>
 where
     JsonRpcClient<T>: Provider,
     T: Send + Sync,
@@ -55,5 +58,27 @@ where
 
     let declaration = account.declare(Arc::new(flattened_class), compiled_class_hash);
 
-    declaration.send().await.map_err(|e| e.to_string())
+    declaration.send().await.map_err(|e| e.to_string())?;
+
+    let constructor_calldata = vec![];
+    let calldata = [
+        vec![
+            compiled_class_hash,                            // class hash
+            FieldElement::ZERO,                             // salt
+            FieldElement::ZERO,                             // unique
+            FieldElement::from(constructor_calldata.len()), // constructor calldata len
+        ],
+        constructor_calldata,
+    ]
+    .concat();
+
+    let txn = account.execute(vec![Call {
+        calldata,
+        selector: selector!("deployContract"),
+        to: felt!("0x041a78e741e5af2fec34b695679bc6891742439f7afb8484ecd7766661ad02bf"),
+    }]);
+
+    txn.send().await.map_err(|e| e.to_string())?;
+
+    Ok(())
 }
