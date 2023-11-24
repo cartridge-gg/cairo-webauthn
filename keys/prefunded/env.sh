3c34f0bf1b0cf1cffadcf2ff2a9e37f7a5cec9cb
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2023-11-24 11:12:24 +0000

    delete keys

--- keys/prefunded/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export STARKNET_KEYSTORE=~/code/cairo-webauthn/keys/prefunded/keystore.json
-export STARKNET_ACCOUNT=~/code/cairo-webauthn/keys/prefunded/account.json
-export STARKNET_RPC=http://0.0.0.0:5050
