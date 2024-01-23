eb5106f78a922515dfaba4b042afefa9bd4ae3e0
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-01-23 11:27:05 +0000

    verify signature tests

--- crates/webauthn/tests/src/auth/mod.rs
+++ crates/webauthn/tests/src/auth/mod.rs
@@ -1,7 +1,39 @@
-use cairo_args_runner::{arg_array, felt_vec, arg_value};
+use cairo_args_runner::{arg_array, arg_value, felt_vec, Arg, Felt252};
 
 mod mod_arithmetic;
 mod verify_signature;
+
+pub struct ArgsBuilder {
+    args: Vec<Arg>,
+}
+
+impl ArgsBuilder {
+    pub fn new() -> Self {
+        Self { args: Vec::new() }
+    }
+    #[allow(dead_code)]
+    pub fn add_one(mut self, arg: impl Into<Felt252>) -> Self {
+        self.args.push(Arg::Value(arg.into()));
+        self
+    }
+    pub fn add_struct(mut self, args: impl IntoIterator<Item = impl Into<Felt252>>) -> Self {
+        self.args
+            .extend(args.into_iter().map(|arg| Arg::Value(arg.into())));
+        self
+    }
+    pub fn add_array(mut self, args: impl IntoIterator<Item = impl Into<Felt252>>) -> Self {
+        self.args
+            .push(Arg::Array(args.into_iter().map(|arg| arg.into()).collect()));
+        self
+    }
+    pub fn build(self) -> Vec<Arg> {
+        self.args
+    }
+}
+
+pub trait FeltSerialize {
+    fn to_felts(self) -> Vec<Felt252>;
+}
 
 #[test]
 fn test_contains_trait() {
@@ -12,7 +44,7 @@ fn test_contains_trait() {
         arg_value!(2),
     ];
     let result = cairo_args_runner::run(target, function, &args).unwrap();
-    
+
     assert_eq!(result.len(), 1);
     assert_eq!(result[0], true.into());
 }
--- crates/webauthn/tests/src/auth/verify_signature.rs
+++ crates/webauthn/tests/src/auth/verify_signature.rs
@@ -1,11 +1,13 @@
-use account_sdk::webauthn_signer::P256r1Signer;
+use account_sdk::webauthn_signer::P256VerifyingKeyConverter;
 use cairo_args_runner::Felt252;
 use p256::{
-    ecdsa::{signature::Signer, SigningKey},
-    elliptic_curve::{consts::P256, rand_core::OsRng},
+    ecdsa::{signature::Signer, Signature, SigningKey},
+    elliptic_curve::rand_core::OsRng,
 };
 
-use crate::{Function, FunctionUnspecified};
+use crate::{Function, FunctionTrait as _, FunctionUnspecified};
+
+use super::{ArgsBuilder, FeltSerialize};
 
 const VERIFY_SIGNATURE: FunctionUnspecified = Function::new_unspecified("verify_signature");
 
@@ -21,20 +23,95 @@ impl CairoU256 {
     pub fn from_bytes_be(low: &[u8; 16], high: &[u8; 16]) -> Self {
         Self::new(Felt252::from_bytes_be(low), Felt252::from_bytes_be(high))
     }
-    pub fn from_byte_slice_be(bytes: &[u8]) -> Self {
-        let (low, high): (&[u8; 16], &[u8; 16]) =
-            (bytes[16..].try_into().unwrap(), bytes[..16].try_into().unwrap());
+    pub fn from_byte_slice_be(bytes: &[u8; 32]) -> Self {
+        let (low, high): (&[u8; 16], &[u8; 16]) = (
+            bytes[16..].try_into().unwrap(),
+            bytes[..16].try_into().unwrap(),
+        );
         Self::from_bytes_be(low, high)
     }
 }
 
-#[test]
-fn test_verify_signature() {
-    let message = b"hello world";
-    let signing_key = SigningKey::random(&mut OsRng);
-    let (signature, _): (_, _) = signing_key.sign(message);
+impl FeltSerialize for CairoU256 {
+    fn to_felts(self) -> Vec<Felt252> {
+        vec![self.low, self.high]
+    }
+}
+
+struct P256r1PublicKey {
+    x: CairoU256,
+    y: CairoU256,
+}
+
+impl P256r1PublicKey {
+    pub fn new(x: CairoU256, y: CairoU256) -> Self {
+        Self { x, y }
+    }
+    pub fn from_bytes_be(x: &[u8; 32], y: &[u8; 32]) -> Self {
+        Self::new(
+            CairoU256::from_byte_slice_be(x),
+            CairoU256::from_byte_slice_be(y),
+        )
+    }
+}
+
+impl FeltSerialize for P256r1PublicKey {
+    fn to_felts(self) -> Vec<Felt252> {
+        self.x
+            .to_felts()
+            .into_iter()
+            .chain(self.y.to_felts())
+            .collect()
+    }
+}
+
+fn verify_signature(
+    hash: &[u8],
+    auth_data: &[u8],
+    signing_key: SigningKey,
+    signature: Signature,
+) -> bool {
     let (r, s) = (signature.r(), signature.s());
     let (r, s) = (r.to_bytes(), s.to_bytes());
     let (r, s) = (r.as_slice(), s.as_slice());
-    let p = signing_key.verifying_key().to_sec1_bytes();
+    let (x, y) = P256VerifyingKeyConverter::new(*signing_key.verifying_key()).to_bytes();
+    let pub_key = P256r1PublicKey::from_bytes_be(&x, &y);
+    let args = ArgsBuilder::new()
+        .add_array(hash.iter().cloned())
+        .add_array(auth_data.iter().cloned())
+        .add_struct(pub_key.to_felts())
+        .add_array(r.into_iter().copied().chain(s.iter().copied()));
+    let result = VERIFY_SIGNATURE.run(args.build());
+    result == vec![0.into(), 0.into()]
+}
+
+#[test]
+fn test_verify_signature_1() {
+    let hash: &[u8] = b"hello world";
+    let auth_data = b"dummy auth data";
+    let signing_key = SigningKey::random(&mut OsRng);
+    let (signature, _) = signing_key.sign(&vec![auth_data, hash].concat());
+    assert!(verify_signature(hash, auth_data, signing_key, signature))
+}
+
+#[test]
+fn test_verify_signature_2() {
+    let hash: &[u8] = b"1234567890987654321";
+    let auth_data = b"auuuuuuuuuuth daaaaataaaaaaaaaa";
+    let signing_key = SigningKey::random(&mut OsRng);
+    let (signature, _) = signing_key.sign(&vec![auth_data, hash].concat());
+    assert!(verify_signature(hash, auth_data, signing_key, signature))
+}
+
+#[test]
+fn test_verify_signature_should_fail_1() {
+    let hash = b"hello world";
+    let auth_data = b"dummy auth data";
+    let wrong_hash: &[u8] = b"definetly not hello world";
+    let signing_key = SigningKey::random(&mut OsRng);
+    let (signature, _) = signing_key.sign(&vec![auth_data, wrong_hash].concat());
+    assert_eq!(
+        verify_signature(hash, auth_data, signing_key, signature),
+        false
+    )
 }
